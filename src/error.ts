718b38d162cbca0d069fefba7c1713ccab4fe07c
Mathieu Acthernoene <zoontek@gmail.com>
2022-02-16 16:24:29 +0100

    Add error

--- src/error.ts
+++ src/error.ts
@@ -1,1 +1,14 @@
-export default {};
+export class EnvError extends Error {
+  constructor(public faultyVariables: string[] = []) {
+    super(
+      faultyVariables.length > 0
+        ? `Some environment variables are missing or are invalid: ${faultyVariables
+            .map((variable) => `"${variable}"`)
+            .join(", ")}`
+        : "Unknown environment error",
+    );
+
+    Object.setPrototypeOf(this, EnvError.prototype);
+    this.name = this.constructor.name;
+  }
+}
