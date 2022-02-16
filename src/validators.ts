85ee3931d576678501db35f87ffe978875faaddf
Mathieu Acthernoene <zoontek@gmail.com>
2022-02-16 16:24:38 +0100

    Add validators

--- src/validators.ts
+++ src/validators.ts
@@ -1,1 +1,13 @@
-export default {};
+export type Validator<T> = (input: string) => T | void;
+
+export const bool: Validator<boolean> = (input) => {
+  if (input === "true") return true;
+  if (input === "false") return false;
+};
+
+export const nbr: Validator<number> = (input) => {
+  const parsed = parseFloat(input);
+  if (!Number.isNaN(parsed)) return parsed;
+};
+
+export const str: Validator<string> = (input) => input;
